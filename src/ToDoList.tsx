5cf75eb02d17577eb0a68dd8677a88420365df51
dong53358 <dong53358@naver.com>
2022-06-13 02:12:43 +0900

    to-do-list start

---
+++ src/ToDoList.tsx
@@ -0,0 +1,5 @@
+function ToDolist() {
+  return <div>start</div>;
+}
+
+export default ToDolist;
---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bdBgColor: "#121212",
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#9c88ff",
+  btnColor: "#0652DD",
+};
