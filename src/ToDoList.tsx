da4dff10fc1e7defaf242d499ea8fd82c20d6f8d
dong53358 <dong53358@naver.com>
2022-06-18 23:23:17 +0900

    form validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,5 +1,69 @@
+import React, { ReactHTMLElement, useState } from "react";
+import { useForm } from "react-hook-form";
+
+/*
 function ToDolist() {
-  return <div>start</div>;
+  const [toDo, setTodo] = useState("");
+  const [toDoError, setToDoError] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setToDoError("");
+    setTodo(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (toDo.length < 10) {
+      return setToDoError("To do should be longer");
+    }
+    console.log("submit");
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+        <button>Add</button>
+        {toDoError !== "" ? toDoError : null}
+      </form>
+    </div>
+  );
+}
+*/
+
+function ToDoList() {
+  const { register, handleSubmit } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onValid)}>
+        <input {...register("email", { required: true })} placeholder="Email" />
+        <input
+          {...register("firstName", { required: true })}
+          placeholder="First Name"
+        />
+        <input
+          {...register("las Name", { required: true })}
+          placeholder="Last Name"
+        />
+        <input
+          {...register("username", { required: true })}
+          placeholder="Username"
+        />
+        <input
+          {...register("password", { required: true })}
+          placeholder="Password"
+        />
+        <input
+          {...register("password1", { required: true })}
+          placeholder="Password1"
+        />
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
 
-export default ToDolist;
+export default ToDoList;
